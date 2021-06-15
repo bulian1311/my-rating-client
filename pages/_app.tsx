df11ebab468154d3829cfdefa11ffa6dc556f026
bulian1311 <bulian1311@gmail.com>
2021-06-15 18:52:32 +0300

    Add _document and init prettier.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My Rating App</title>
+        <meta name="description" content="My rating app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -65,5 +65,5 @@ export default function Home(): JSX.Element {
         </a>
       </footer>
     </div>
-  )
+  );
 }
